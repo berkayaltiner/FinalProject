05d2f23fceeffde5ade9dc848b96595f02e6954d
berkayaltiner <berkay.altiner.98@gmail.com>
2021-02-22 17:31:53 +0200

    Entity Framework installed

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint --->  where T:class , Class means it is a referance type, not the general class we know.
+    //where T:class,IEntity ---> It is only a "IEntity" or an object that implements "IEntity".
+    //new(): it should be like "new Product()". 
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
