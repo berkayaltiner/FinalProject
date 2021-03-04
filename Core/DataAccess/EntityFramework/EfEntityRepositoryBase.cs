f7ac187f0814c255ea53fa1b733a341a407e750a
berkayaltiner <berkay.altiner.98@gmail.com>
2021-03-05 01:54:18 +0200

    Base repository created.
    Base repository created in Core layer.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,70 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase <TEntity,TContext> : IEntityRepository<TEntity>
+        where TEntity: class,IEntity,new()
+        where TContext: DbContext,new() // contains inherited class from DbContext.
+    {
+        public void Add(TEntity entity)
+        {
+            //IDisposable pattern implementation of C#
+            //The Context object takes up a lot of memory.
+            //We use "using ()" to increase the performance speed of the system. Quickly clears memory.
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                /* ----Ternary operation----
+                   Before the "?" sign, we define a condition like below ( filter == null )
+                   If Given condition is true, Left side which is before " : " sign works.
+                   If Given condition is false, Right side which is after " : " sign works.
+                 */
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
