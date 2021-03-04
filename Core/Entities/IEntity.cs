38060b4ede9245d3b4a576f72b929a3a784e1b02
berkayaltiner <berkay.altiner.98@gmail.com>
2021-03-05 01:56:58 +0200

    IEntity was moved Core layer.
    IEntity was moved Core layer.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+ 
+
+    //The class that implements from "IEntity" is a database table or object.
+    public interface IEntity
+    {
+    }
+}
