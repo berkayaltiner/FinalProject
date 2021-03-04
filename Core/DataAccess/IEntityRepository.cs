0b0bae711c85566c4e6d2c9bc8f7efe051445038
berkayaltiner <berkay.altiner.98@gmail.com>
2021-03-05 01:56:10 +0200

    IEntityRepository carried out Core layer.
    IEntityRepository was moved Core layer.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //generic constraint --->  where T:class , Class means it is a referance type, not the general class we know.
+    //where T:class,IEntity ---> It is only a "IEntity" or an object that implements "IEntity".
+    //new(): it should be like "new Product()". 
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
